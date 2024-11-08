ed624ea9066e13b7352abb89808bba9cff665230
FAB456V <161176464+FAB456V@users.noreply.github.com>
2024-11-08 15:07:09 -0500

    8/11/24

--- app/src/main/java/com/example/paperos2app/Activity/LoginActivity.java
+++ app/src/main/java/com/example/paperos2app/Activity/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.paperos2app.Activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +14,17 @@ import com.example.paperos2app.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText usuario , contrasena;
+    Button btn_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login);
+      usuario= findViewById(R.id.emailEditText);
+      contrasena= findViewById(R.id.passwordEditText);
+      btn_login= findViewById(R.id.btn_login);
 
     }
 }
